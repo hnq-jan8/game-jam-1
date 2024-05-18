5de899395c07e0d45ba938acbe5259b73c2daa76
Nguyen Vu Cuong <thecoldsea0412@gmail.com>
2024-05-19 02:07:00 +0700

    Added Gun Upgrade

--- Assets/Scripts/Global/Player.cs
+++ Assets/Scripts/Global/Player.cs
@@ -6,15 +6,58 @@ public class Player : MonoBehaviour
 {
     public static Player Instance { get; private set; }
 
+    [SerializeField]
+    private List<Gun> gunList;
+
+    private int currentGunIndex;
+
+    [SerializeField]
+    private int currentEnergy;
+
+    [SerializeField]
+    private int gunReloadCountDown;
+
+    [SerializeField]
+    private
+
     // Start is called before the first frame update
     void Start()
     {
-        Instance = this;   
+        Instance = this;
+
+        currentEnergy = gunList[0].Energy;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpgradeGun()
+    {
+        if (currentGunIndex >= gunList.Count) return;
+
+        currentGunIndex++;
+
+        currentEnergy = gunList[currentGunIndex].Energy;
+        //Should also change gun skin and gun bullet skin
+    }
+
+    public void UseEnergy()
+    {
+        currentEnergy--;
+    }
+
+    public void ReloadGun()
+    {
+        StartCoroutine(ReloadCountdown(gunList[currentGunIndex].ReloadTime));
+    }
+
+    private IEnumerator ReloadCountdown(float reloadTime)
+    {
+        yield return new WaitForSeconds(reloadTime);
+
+        currentEnergy = gunList[currentGunIndex].Energy;
     }
 }
